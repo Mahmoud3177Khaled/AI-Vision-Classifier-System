0b3bd02a5b88be42ee02460bfd929315900968e9
Salma884 <147331892+Salma884@users.noreply.github.com>
2025-12-14 20:18:35 +0200

    Update train_svm.py

---
+++ src/train_svm.py
@@ -0,0 +1,29 @@
+from scipy.stats import loguniform
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+from pca import *
+import numpy as np
+from sklearn.svm import SVC
+
+# Load preprocessing objects
+
+X_train, y_train, X_test, y_test = get_data(features_dir)
+
+X_train, X_test = scale_PCA(
+    scaler, pca, X_train, X_test, y_train
+)
+
+
+svm = SVC(
+    C=10,
+    gamma='scale',
+    kernel='rbf',
+    class_weighxt='balanced',
+    decision_function_shape='ovr'
+)
+
+svm.fit(X_train, y_train)
+
+# Evaluate on test set
+test_acc = svm.score(X_test, y_test)
+print("Test accuracy:", test_acc)
